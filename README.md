c89adc55b60c93e4645344fe9890e12cf20429a9
Priti Parkar <pritidc@gmil.com>
2013-11-11 12:52:03 -0800

    adding files..

---
+++ README.md
@@ -0,0 +1,28 @@
+
+
+# HouseApplication
+
+
+
+## Usage
+
+
+
+## Developing
+To run the application follow the steps below,
+
+1. Start the nodejs server from nodejs prompt
+>node controlHouse.js
+
+2. To test using 'post' request, load 'house.html' in browser
+Eg:  file:///C:/Documents%20and%20Settings/pparkar/HouseApplication/house.html
+
+Set the temperature to desired value using the post request. The new temperature will be saved in the config file named as "houseConfig.json" 
+
+3. To test using 'get' request, load the url below in the browser,
+http://localhost:8000/temperature?value=65
+
+
+### Tools
+Nodejs
+JQuery
---
+++ controlHouse.js
@@ -0,0 +1,89 @@
+/**
+Global Include
+Prerequisite: npm install connect
+*/
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
+
+function loadConfig(newTemp,callback){
+var data = fs.readFileSync('./houseConfig.json'), 
+	myObj;
+try	{
+	myObj = JSON.parse(data);
+	console.dir(myObj);
+	if(typeof myObj['temperature'] !== 'undefined'){
+		var oldTemp = myObj['temperature']; 
+//		console.log('Old temperature='+oldTemp);
+
+		if(newTemp == oldTemp)
+			callback("Temperature is already set at "+newTemp);
+		else
+			callback("Setting temperature to "+newTemp);				
+	}else{
+		callback("Error in fetching existing state, temperature will be set to new value "+newTemp);
+	}
+	
+	//set new temperature in config file
+	myObj.temperature=newTemp;
+	fs.writeFile('./houseConfig.json', JSON.stringify(myObj), function (err) {
+	    if (err) {
+	      console.log('Error setting new temperature in config file');
+	      console.log(err.message);
+	      return;
+	    }
+	    console.log('Temperature set successfully');
+	  });
+}
+catch (err) {
+	console.log('There has been an error parsing your JSON.');
+	console.log(err);
+}
+}
+
+/**
+Create Server On port 8000
+EG: http://localhost:8000/temperature?value=65
+*/
+http.createServer(function (request, response) {
+response.writeHead(200, {'Content-Type': 'text/html','Access-Control-Allow-Origin' : '*'});
+if(request.method == 'POST'){
+   var req_data = '';
+   request.on('data', function(data){
+	  req_data += data; 
+   });
+
+   request.on('end', function(){
+	  console.log('data:'+req_data);
+	  var req_obj = qs.parse(req_data);
+	  for(var key in req_obj){
+//		  console.log("***"+key+"  "+req_obj[key])
+		  if(key == "temperature"){
+				loadConfig(req_obj[key],function(callback){
+					console.log(req_obj[key]+","+callback);
+					response.end(callback,'utf-8');
+				});
+		  }
+	  }
+   });
+}else{
+	var entity = url.parse(request.url).pathname;
+	var value = url.parse(request.url).query;
+	console.log(entity+"  "+value);
+
+	var eachParam = entity.split("\/");
+	console.log("entity to compare "+eachParam[1]);
+	if(eachParam.length>0 && eachParam[1] && (eachParam[1] == "temperature")){
+		var newTemp = value.split("=");
+		loadConfig(newTemp,function(callback){
+//			console.log(eachParam[1]+","+callback);
+			response.end(callback,'utf-8');
+		});
+	}else{
+		response.end("Please check URL, Correct URL is http://localhost:8000/temperature?value=65");
+	}
+}
+}).listen(8000, '127.0.0.1');
+
+console.log('Server running at 8000');
